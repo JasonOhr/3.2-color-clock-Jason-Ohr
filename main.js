3f4449676094d7f6a7ee02fc11a1a98df116a8be
Jason Ohr <jason.ohr@gmail.com>
2015-05-19 15:56:30 -0400

    Added more files

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by firewaterjoe on 5/19/15.
+ */
+
+function startTime() {
+  var date = new Date();
+
+}
+function currentTime(startTime) {
+  var dateNow = new Date();
+    var time
+}
